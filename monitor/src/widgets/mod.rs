bbe1ecdebedb10e095467dd3c89d10669a520621
Sebastian Woetzel <wose@zuendmasse.de>
2019-10-23 20:08:44 +0200

    adds viridis color map and waterfall widget

--- monitor/src/widgets/mod.rs
+++ monitor/src/widgets/mod.rs
@@ -1,3 +1,6 @@
 mod info_bar;
+mod viridis;
+mod waterfall;
 
 pub use self::info_bar::InfoBar;
+pub use self::waterfall::Waterfall;
---
+++ monitor/src/widgets/waterfall.rs
@@ -0,0 +1,19 @@
+use super::viridis::VIRIDIS;
+
+use tui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::{Color, Style},
+    widgets::{Block, Widget},
+};
+
+pub struct Waterfall<'a> {
+    block: Option<Block<'a>>,
+    
+}
+
+impl<'a> Widget for Waterfall {
+    fn draw(&mut self, area: Rect, buf: &mut Buffer) {
+        
+    }
+}
